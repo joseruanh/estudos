410f79ec861e57465f36a23a1c3fa42516f3c7d4
Ruanh <79330021+joseruanh@users.noreply.github.com>
2024-03-04 12:08:29 -0300

    att

---
+++ js/system.js
@@ -0,0 +1,19 @@
+function matematica()
+{
+    let a = window.document.getElementById("res")
+    a.innerHTML = `
+    
+
+    <p class="assuntos">
+                Conteúdos iniciais para Matemática:
+                
+                <br>Conceitos básicos <br><br>
+                <br><br>Adição <br><br> <a href="https://www.youtube.com/watch?v=WQSeNLlLjiU" target="_blank"><button class="videosbt" id="adicao">Adição</button></a>
+                <br><br><br>Subtração <br><br> <a href="https://www.youtube.com/watch?v=fy3CBP0lu8M" target="_blank"> <button class="videosbt" id="sub">Subtração</button> </a>
+                <br><br><br>Multiplicação <br><br> <a href="https://www.youtube.com/watch?v=izaSmMXZRgk" target="_blank"><button class="videosbt" id="mult">Multiplicação</button></a>
+                <br><br><br>Divisão <br><br> <a href="https://www.youtube.com/watch?v=61S-Rayhc7c" target="_blank"> <button class="videosbt" id="divi">Divisão</button></a>
+            </p>
+    
+    
+    `
+}
