aa2fbfacfb7cffee6a40eceaf035a180ef16e504
Ruanh <79330021+joseruanh@users.noreply.github.com>
2024-03-04 12:13:35 -0300

    att

--- js/system.js
+++ js/system.js
@@ -8,10 +8,10 @@ function matematica()
                 Conteúdos iniciais para Matemática:
                 
                 <br>Conceitos básicos <br><br>
-                <br><br>Adição <br><br> <a href="https://www.youtube.com/watch?v=WQSeNLlLjiU" target="_blank"><button class="videosbt" id="adicao">Adição</button></a>
-                <br><br><br>Subtração <br><br> <a href="https://www.youtube.com/watch?v=fy3CBP0lu8M" target="_blank"> <button class="videosbt" id="sub">Subtração</button> </a>
-                <br><br><br>Multiplicação <br><br> <a href="https://www.youtube.com/watch?v=izaSmMXZRgk" target="_blank"><button class="videosbt" id="mult">Multiplicação</button></a>
-                <br><br><br>Divisão <br><br> <a href="https://www.youtube.com/watch?v=61S-Rayhc7c" target="_blank"> <button class="videosbt" id="divi">Divisão</button></a>
+                <br><br>Adição <br><br> <a href="https://www.youtube.com/watch?v=WQSeNLlLjiU" target="_blank"><button class="videosbt" id="adicao">-</button></a>
+                <br><br><br>Subtração <br><br> <a href="https://www.youtube.com/watch?v=fy3CBP0lu8M" target="_blank"> <button class="videosbt" id="sub">-</button> </a>
+                <br><br><br>Multiplicação <br><br> <a href="https://www.youtube.com/watch?v=izaSmMXZRgk" target="_blank"><button class="videosbt" id="mult">-</button></a>
+                <br><br><br>Divisão <br><br> <a href="https://www.youtube.com/watch?v=61S-Rayhc7c" target="_blank"> <button class="videosbt" id="divi">-</button></a>
             </p>
     
     
